9be33d815a52b13ebc766743fd0cd1323c4b921e
JohnGR025 <ioannisvourvouriotis@yahoo.com>
2025-12-23 21:18:04 +0200

    Starting files

---
+++ main.c
@@ -0,0 +1,21 @@
+/*  
+    Developer's Info:
+    Ioannis-Nektarios Bourbouriotis
+    A.M: 2022202400025
+    dit24025@go.uop.gr
+
+    Alexandros Papadopoulos
+    A.M: 2022202400156
+    dit24156@go.uop.gr
+*/
+
+//Libraries
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
+//Function Factory
