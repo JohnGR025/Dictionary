a2dd90fb4aac8965df3c1d71fc96bd3b6173742d
JohnGR025 <ioannisvourvouriotis@yahoo.com>
2025-12-24 15:24:07 +0200

    updated main file

--- main.c
+++ main.c
@@ -10,21 +10,74 @@
 */
 
 //Libraries
+#include <stdio.h>
 
+//Static variables for general use
 static dictionary_size = 0;
+static storing_type = 1;
 
 int main(int argc, char const *argv[])
 {
+    //Checking starting input
+    if (argc<=1 || argc>3) //Wrong start input, wrong num of inputs
+        wrongStartInput();
+    else
+    {
+        if (argc==2)
+        {
+            //Determine counting mode
+            if (strlen(argv[1])==1 && argv[1][0]=='1')
+                dictionary_size = countWordsType1("dictionary");
+            else if (strlen(argv[1])==1 && argv[1][0]=='2')
+                dictionary_size = countWordsType2("dictionary");
+            else
+                wrongStartInput();
+            storing_type = argv[1]; //Set the way of dictionary's storing type
+            
+        }
+        else if (argc==3)
+        {
+            //Determine counting mode
+            if (strlen(argv[2])==1 && argv[2][0]=='1')
+                dictionary_size = countWordsType1("dictionary");
+            else if (strlen(argv[2])==1 && argv[2][0]=='2')
+                dictionary_size = countWordsType2("dictionary");
+            else
+                wrongStartInput();
+            storing_type = argv[2]; //Set the way of dictionary's storing type
+        }
+    }
+
+    //Variables
     
     return 0;
 }
 
 //Function Factory
-unsigned int strtohash(char* str){
+unsigned int strToHash(char* str)
+{
      unsigned int hash=0;
      while(*str){
         hash=*str+31*hash;
         str++;
      }
      return hash%dictionary_size;
+}
+
+int countWordsType1(FILE* fname)
+{
+    int count=0;
+}
+
+int countWordsType2(FILE* fname)
+{
+
+}
+
+void wrongStartInput(void)
+{
+    printf("You need to start the program as follows:\n");
+    printf("./main <file_name> [1 or 2]\n");
+    printf("Read the README for info, fellow user:)\n\n");
+    exit(0); //Stop program
 }
